7b4f7b37b894017c0722c91fffadd210198150f6
Justin Lopato <justin@lopato.org>
2026-02-20 22:49:28 -0600

    Fix variable syntax in customize.bash script

--- .github/workflows/customize.bash
+++ .github/workflows/customize.bash
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
-echo `
+echo "$(cat << EOM
 user:deluser=pi
-user:adduser=${WS_NAME}|password=${WS_NAME}
+user:adduser=$WS_NAME|password=$WS_NAME
 apt-addrepo:repo=deb http://packages.ros.org/ros2/ubuntu/ noble|gpgkey=https://github.com/ros-infrastructure/ros-apt-source/raw/refs/heads/main/ros-apt-source/keys/ros2-archive-keyring.gpg
 apps:apps=ros-jazzy-desktop
 network:ifname=wlan0|wifissid=PBS4|wifipassword=DPS2home01|wificountry=US
 network:ifname=eth0
 L10n:keymap=us|locale=en_US.UTF-8|timezone=America/Chicago|wificountry=us
 disables:piwiz|cloudinit
-sshkey:sshuser=${WS_NAME}|import-key=./id_ed25519|passphrase=
-git-clone:repo=git@github.com:${REPO}.git|gitdir=/home/${WS_NAME}/${REPO_NAME}|user=${WS_NAME}
-` >> ./my.plugins
+sshkey:sshuser=$WS_NAME|import-key=./id_ed25519|passphrase=
+git-clone:repo=git@github.com:$REPO.git|gitdir=/home/$WS_NAME/$REPO_NAME|user=$WS_NAME
+EOM
+)" >> ./my.plugins
 
 sudo sdm --customize --plugin @./my.plugins --extend --xmb 2048 --restart --logwidth 256 ubuntu.img
