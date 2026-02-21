43b2ca22f4c29d47413246bcc6047a8b02a8af2a
Justin Lopato <justin@lopato.org>
2026-02-21 02:34:40 -0600

    Update network configuration in customize.bash

--- .github/workflows/customize.bash
+++ .github/workflows/customize.bash
@@ -6,7 +6,8 @@ user:deluser=pi
 user:adduser=$WS_NAME|password=$WS_NAME
 # apt-addrepo:repo=deb http://packages.ros.org/ros2/ubuntu/ noble|gpgkey=http://packages.ros.org/ros2/ubuntu/dists/noble/Release.gpg
 # apps:apps=ros-jazzy-desktop
-network:netman=network-manager|ifname=wlan0|wifissid=PBS4|wifipassword=DPS2home01|wificountry=US|ifname=eth0
+network:netman=network-manager|ifname=wlan0|wifissid=PBS4|wifipassword=DPS2home01|wificountry=US
+network:netman=network-manager|ifname=eth0
 L10n:keymap=us|locale=en_US.UTF-8|timezone=America/Chicago|wificountry=us
 disables:piwiz|cloudinit
 sshkey:sshuser=$WS_NAME|import-key=./id_ed25519|passphrase=
