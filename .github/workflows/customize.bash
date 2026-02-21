1dd2e44c3f4924fb03a9f83249caf6eace4de43b
Justin Lopato <justin@lopato.org>
2026-02-20 22:44:39 -0600

    Modify customize.bash to update plugin generation

--- .github/workflows/customize.bash
+++ .github/workflows/customize.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-(cat <<'EOF'
+echo `
 user:deluser=pi
 user:adduser=${WS_NAME}|password=${WS_NAME}
 apt-addrepo:repo=deb http://packages.ros.org/ros2/ubuntu/ noble|gpgkey=https://github.com/ros-infrastructure/ros-apt-source/raw/refs/heads/main/ros-apt-source/keys/ros2-archive-keyring.gpg
@@ -12,7 +12,6 @@ L10n:keymap=us|locale=en_US.UTF-8|timezone=America/Chicago|wificountry=us
 disables:piwiz|cloudinit
 sshkey:sshuser=${WS_NAME}|import-key=./id_ed25519|passphrase=
 git-clone:repo=git@github.com:${REPO}.git|gitdir=/home/${WS_NAME}/${REPO_NAME}|user=${WS_NAME}
-EOF
-    ) | bash -c "cat >> ./my.plugins"
+` >> ./my.plugins
 
 sudo sdm --customize --plugin @./my.plugins --extend --xmb 2048 --restart --logwidth 256 ubuntu.img
