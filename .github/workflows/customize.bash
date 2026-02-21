382164b6c7dc4abee058580eeae2fde6d828e742
Justin Lopato <justin@lopato.org>
2026-02-20 22:40:23 -0600

    Update customize.bash

--- .github/workflows/customize.bash
+++ .github/workflows/customize.bash
@@ -6,7 +6,7 @@ user:deluser=pi
 user:adduser=${WS_NAME}|password=${WS_NAME}
 apt-addrepo:repo=deb http://packages.ros.org/ros2/ubuntu/ noble|gpgkey=https://github.com/ros-infrastructure/ros-apt-source/raw/refs/heads/main/ros-apt-source/keys/ros2-archive-keyring.gpg
 apps:apps=ros-jazzy-desktop
-network:ifname=wlan0|PBS4|wifipassword=DPS2home01|wificountry=US
+network:ifname=wlan0|wifissid=PBS4|wifipassword=DPS2home01|wificountry=US
 network:ifname=eth0
 L10n:keymap=us|locale=en_US.UTF-8|timezone=America/Chicago|wificountry=us
 disables:piwiz|cloudinit
